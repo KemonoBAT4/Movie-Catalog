7b4bbb59e772b149394f8d0652867696a7fedb7e
KemonoBAT4 <kemono.bat.4@gmail.com>
2026-07-08 01:09:11 +0200

    [backend] minor changes to the files

--- backend/core/routes/_common.py
+++ backend/core/routes/_common.py
@@ -4,7 +4,7 @@
 from flask import Blueprint, redirect, url_for, request, jsonify                     # type: ignore
 from flask import render_template, send_from_directory                               # type: ignore
 from flask_login import login_required                                               # type: ignore
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, set_access_cookies, unset_jwt_cookies
 from sqlalchemy import and_, or_
 
 # core imports
