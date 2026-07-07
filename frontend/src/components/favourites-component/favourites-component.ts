e76902c1f58a633b7bdee21217d10dd530a0bcb3
KemonoBAT4 <kemono.bat.4@gmail.com>
2026-07-08 00:59:33 +0200

    [frontend] added favourites component

---
+++ frontend/src/components/favourites-component/favourites-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favourites-component',
+  imports: [],
+  templateUrl: './favourites-component.html',
+  styleUrl: './favourites-component.css',
+})
+export class FavouritesComponent {}
