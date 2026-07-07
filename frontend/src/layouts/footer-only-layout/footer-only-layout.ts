4a91429dd3f7bf493cee472a8ee3069625acd0db
KemonoBAT4 <kemono.bat.4@gmail.com>
2026-07-08 01:02:16 +0200

    [frontend] added a footer only layout for settings pages or similar

---
+++ frontend/src/layouts/footer-only-layout/footer-only-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-only-layout',
+  imports: [],
+  templateUrl: './footer-only-layout.html',
+  styleUrl: './footer-only-layout.css',
+})
+export class FooterOnlyLayout {}
