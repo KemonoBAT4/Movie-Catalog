0e50e2454566b923354c966c312b89baaf9aa0d5
KemonoBAT4 <kemono.bat.4@gmail.com>
2026-07-08 01:04:10 +0200

    [frontend] added the main home page (or landing page)

--- frontend/src/pages/homepage/homepage.ts
+++ frontend/src/pages/homepage/homepage.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { ContinueWatchingComponent } from '../../components/continue-watching-component/continue-watching-component';
+import { HeroComponent } from '../../components/hero-component/hero-component';
+import { FavouritesComponent } from '../../components/favourites-component/favourites-component';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [
+    ContinueWatchingComponent,
+    HeroComponent,
+    FavouritesComponent
+  ],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
 })
