536b9c1b55435e79439c760999e672f2035a457b
KemonoBAT4 <kemono.bat.4@gmail.com>
2026-07-19 02:45:16 +0200

    [frontend] added the widgets to the homepage and fixed the css

--- frontend/src/pages/homepage/homepage.ts
+++ frontend/src/pages/homepage/homepage.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { ContinueWatchingComponent } from '../../components/continue-watching-component/continue-watching-component';
 import { HeroComponent } from '../../components/hero-component/hero-component';
 import { FavouritesComponent } from '../../components/favourites-component/favourites-component';
+import { RecentlyAddedComponent } from '../../components/recently-added-component/recently-added-component';
 
 @Component({
   selector: 'app-homepage',
   imports: [
     ContinueWatchingComponent,
     HeroComponent,
-    FavouritesComponent
+    FavouritesComponent,
+    RecentlyAddedComponent
   ],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
