b2b2f1b77aa033d4a343ecd4aafa9a6120e2e50c
KemonoBAT4 <kemono.bat.4@gmail.com>
2026-07-19 02:44:14 +0200

    [frontend] implemented "favourites" widget with some static items

--- frontend/src/components/favourites-component/favourites-component.ts
+++ frontend/src/components/favourites-component/favourites-component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
 
+interface MovieCard {
+  title: string;
+  posterUrl: string;
+  rating: number; // es. 4.8
+}
+
 @Component({
   selector: 'app-favourites-component',
   imports: [],
   templateUrl: './favourites-component.html',
   styleUrl: './favourites-component.css',
 })
-export class FavouritesComponent {}
+export class FavouritesComponent {
+  movies: MovieCard[] = [
+    { title: 'Interstellar' , posterUrl: 'https://s3.zerochan.net/Zhuang.Fangyi.240.4691020.avif', rating: 4.8 },
+    { title: 'Inception'    , posterUrl: 'https://s3.zerochan.net/Zhuang.Fangyi.240.4691020.avif', rating: 4.8 },
+    { title: 'Parasite'     , posterUrl: 'https://s3.zerochan.net/Zhuang.Fangyi.240.4691020.avif', rating: 4.8 },
+    { title: 'Pulp Fiction' , posterUrl: 'https://s3.zerochan.net/Zhuang.Fangyi.240.4691020.avif', rating: 4.8 },
+    { title: 'Spirited Away', posterUrl: 'https://s3.zerochan.net/Zhuang.Fangyi.240.4691020.avif', rating: 4.6 },
+  ];
+
+  stars = [1, 2, 3, 4, 5];
+}
