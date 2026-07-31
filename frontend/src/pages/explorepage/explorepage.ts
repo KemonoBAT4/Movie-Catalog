ff057bbc3909ded61baccd057174954a41391e65
KemonoBAT4 <kemono.bat.4@gmail.com>
2026-07-31 18:21:21 +0200

    [frontend] added explorepage component

---
+++ frontend/src/pages/explorepage/explorepage.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-explorepage',
+  imports: [],
+  templateUrl: './explorepage.html',
+  styleUrl: './explorepage.css',
+})
+export class Explorepage {}
